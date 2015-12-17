d602b552936007680659956b4e91dd77ee95454f
Mark Bradley <braddle@gmail.com>
2015-12-17 10:10:31 +0000

    Adding error handling to roman numeral converter

--- convert.go
+++ convert.go
@@ -1,80 +1,97 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
 )
 
 type RomanNumeral struct {
-  numeral string
+	numeral string
 }
 
-func  NewRomanNumeral(numeral string) RomanNumeral  {
-  rn := RomanNumeral { numeral }
-  // rn.numeral = numeral
+func NewRomanNumeral(numeral string) (RomanNumeral, error) {
+	numeral = strings.ToUpper(strings.Join(strings.Fields(numeral), ""))
 
-  return rn
+	invalid, err := regexp.MatchString("[^I|V|X|L|C|D|M]", numeral)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if invalid {
+		return RomanNumeral{}, errors.New("Invalid characters in roman numeral. Valid roman numerals are I, V, X, L, C, D and M")
+	}
+
+	rn := RomanNumeral{numeral}
+	// rn.numeral = numeral
+
+	return rn, nil
 }
 
 func (r RomanNumeral) String() string {
-  return r.numeral
+	return r.numeral
 }
 
 func (r RomanNumeral) asSlice() []string {
-  return strings.Split(r.numeral, "")
+	return strings.Split(r.numeral, "")
 }
 
 func main() {
-  numeralString := os.Args[1]
-  numeral := NewRomanNumeral(numeralString)
-  number := convertToInteger(numeral)
+	numeralString := os.Args[1]
+	numeral, err := NewRomanNumeral(numeralString)
 
-  fmt.Printf("%v = %v\n", numeral, number)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	number := convertToInteger(numeral)
+
+	fmt.Printf("%v = %v\n", numeral, number)
 }
 
 func convertToInteger(rn RomanNumeral) int {
-  total := 0
-  var previous int
+	total := 0
+	var previous int
 
-  numerals := reverse(rn.asSlice())
+	numerals := reverse(rn.asSlice())
 
-  for _, numeral := range numerals {
-    value := getNumeralValue(numeral)
+	for _, numeral := range numerals {
+		value := getNumeralValue(numeral)
 
-    fmt.Printf("%v\n", previous)
-    if (previous <= value) {
-      total = total + value
-    } else {
-      total = total - value
-    }
+		if previous <= value {
+			total = total + value
+		} else {
+			total = total - value
+		}
 
-    previous = value
+		previous = value
+	}
 
-  }
-
-  // fmt.Printf("%v", numerals)
-  return total
+	return total
 }
 
 func reverse(sl []string) []string {
-  for i := len(sl)/2-1; i >= 0; i-- {
-    opp := len(sl)-1-i
-    sl[i], sl[opp] = sl[opp], sl[i]
-  }
+	for i := len(sl)/2 - 1; i >= 0; i-- {
+		opp := len(sl) - 1 - i
+		sl[i], sl[opp] = sl[opp], sl[i]
+	}
 
-  return sl
+	return sl
 }
 
 func getNumeralValue(n string) int {
-  numeralValues := make(map[string]int)
-  numeralValues["I"] = 1
-  numeralValues["V"] = 5
-  numeralValues["X"] = 10
-  numeralValues["L"] = 50
-  numeralValues["C"] = 100
-  numeralValues["D"] = 500
-  numeralValues["M"] = 1000
+	numeralValues := make(map[string]int)
+	numeralValues["I"] = 1
+	numeralValues["V"] = 5
+	numeralValues["X"] = 10
+	numeralValues["L"] = 50
+	numeralValues["C"] = 100
+	numeralValues["D"] = 500
+	numeralValues["M"] = 1000
 
-  return numeralValues[n]
+	return numeralValues[n]
 }
