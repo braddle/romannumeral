5b2ff3b213af3decbb7fbedde53f6ae17a00e517
Mark Bradley <braddle@gmail.com>
2015-12-18 11:04:53 +0000

    Moving the roman numeral functionality to its own package.

--- convert.go
+++ convert.go
@@ -1,64 +1,29 @@
-package main
+package romannumeral
 
 import (
 	"errors"
-	"fmt"
-	"log"
-	"os"
 	"regexp"
 	"strings"
 )
 
-type RomanNumeral struct {
-	numeral string
-}
-
-func NewRomanNumeral(numeral string) (RomanNumeral, error) {
-	numeral = strings.ToUpper(strings.Join(strings.Fields(numeral), ""))
-
-	invalid, err := regexp.MatchString("[^I|V|X|L|C|D|M]", numeral)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if invalid {
-		return RomanNumeral{}, errors.New("Invalid characters in roman numeral. Valid roman numerals are I, V, X, L, C, D and M")
-	}
-
-	rn := RomanNumeral{numeral}
-
-	return rn, nil
-}
+type RomanNumeral string
 
 func (r RomanNumeral) String() string {
-	return r.numeral
+	return string(r)
 }
 
 func (r RomanNumeral) asSlice() []string {
-	return strings.Split(r.numeral, "")
+	return strings.Split(string(r), "")
 }
 
-func main() {
-	numeralString := os.Args[1]
-	numeral, err := NewRomanNumeral(numeralString)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	number := convertToInteger(numeral)
-
-	fmt.Printf("%v = %v\n", numeral, number)
-}
-
-func convertToInteger(rn RomanNumeral) int {
+func (r RomanNumeral) ToInt() int {
 	total := 0
 	var previous int
 
-	numerals := reverse(rn.asSlice())
+	numerals := r.asSlice()
 
-	for _, numeral := range numerals {
+	for i := len(numerals) - 1; i >= 0; i-- {
+		numeral := numerals[i]
 		value := getNumeralValue(numeral)
 
 		if previous <= value {
@@ -73,24 +38,36 @@ func convertToInteger(rn RomanNumeral) int {
 	return total
 }
 
-func reverse(sl []string) []string {
-	for i := len(sl)/2 - 1; i >= 0; i-- {
-		opp := len(sl) - 1 - i
-		sl[i], sl[opp] = sl[opp], sl[i]
+func getNumeralValue(n string) int {
+	switch n {
+	case "I":
+		return 1
+	case "V":
+		return 5
+	case "X":
+		return 10
+	case "L":
+		return 50
+	case "C":
+		return 100
+	case "D":
+		return 500
+	case "M":
+		return 1000
+	default:
+		return 0
 	}
-
-	return sl
 }
 
-func getNumeralValue(n string) int {
-	numeralValues := make(map[string]int)
-	numeralValues["I"] = 1
-	numeralValues["V"] = 5
-	numeralValues["X"] = 10
-	numeralValues["L"] = 50
-	numeralValues["C"] = 100
-	numeralValues["D"] = 500
-	numeralValues["M"] = 1000
+func NewRomanNumeral(numeral string) (RomanNumeral, error) {
+	numeral = strings.ToUpper(strings.Join(strings.Fields(numeral), ""))
 
-	return numeralValues[n]
+	reg := regexp.MustCompile("[^I|V|X|L|C|D|M]")
+	invalid := reg.MatchString(numeral)
+
+	if invalid {
+		return RomanNumeral(""), errors.New("Invalid characters in roman numeral. Valid roman numerals are I, V, X, L, C, D and M")
+	}
+
+	return RomanNumeral(numeral), nil
 }
--- convert_test.go
+++ convert_test.go
@@ -1,4 +1,4 @@
-package main
+package romannumeral
 
 import (
 	"fmt"
@@ -44,12 +44,16 @@ func TestNewRomanNumeralDoesNotAcceptInvalidCharacters(t *testing.T) {
 	}
 }
 
-func TestReverseReversesTheOrderOfTheASlice(t *testing.T) {
-	givenSlice := []string{"abc", "def", "ghi"}
-	expectedSlice := []string{"ghi", "def", "abc"}
-	reversedSlice := reverse(givenSlice)
+func TestNewRomanNumeralStripsSpaces(t *testing.T) {
+	rn, err := NewRomanNumeral(" V I I ")
 
-	assert.Equal(t, expectedSlice, reversedSlice)
+	if err != nil {
+		t.Fatal(fmt.Sprintf("NewRomanNumeral returned an error: %v", err))
+	}
+
+	if rn.String() != "VII" {
+		t.Fatal(fmt.Sprintf("NewRomanNumeral return with unxepected numeral value expected: VII actual: %v", rn))
+	}
 }
 
 func TestGetNumeralValueForIis1(t *testing.T) {
@@ -80,20 +84,24 @@ func TestGetNumeralValueForMis1000(t *testing.T) {
 	assert.Equal(t, 1000, getNumeralValue("M"))
 }
 
+func TestGetNumeralValueForInvalidNumeralIs0(t *testing.T) {
+	assert.Equal(t, 0, getNumeralValue("A"))
+}
+
 func TestConvertToIntegerXVis15(t *testing.T) {
 	rn, _ := NewRomanNumeral("XV")
 
-	assert.Equal(t, 15, convertToInteger(rn))
+	assert.Equal(t, 15, rn.ToInt())
 }
 
 func TestConvertToIntegerXIVis14(t *testing.T) {
 	rn, _ := NewRomanNumeral("XIV")
 
-	assert.Equal(t, 14, convertToInteger(rn))
+	assert.Equal(t, 14, rn.ToInt())
 }
 
 func TestConvertToIntegerMCMXCVIIIis1998(t *testing.T) {
 	rn, _ := NewRomanNumeral("MCMXCVIII")
 
-	assert.Equal(t, 1998, convertToInteger(rn))
+	assert.Equal(t, 1998, rn.ToInt())
 }
